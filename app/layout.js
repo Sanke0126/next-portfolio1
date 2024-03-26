72776c721dd7b3aa47f774afcfa6cbaa81980c3f
Sanke0126 <146428956+Sanke0126@users.noreply.github.com>
2024-03-26 12:34:51 +0800

    woooo

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,16 @@
 import "./globals.css";
-import Navbar from "../Components/header";
-import Main from "../Components/main";
-import About from "@/Components/about";
-import Resume from "../Components/resume";
-import Tools from "@/Components/tools";
-import Contact from "@/Components/contact";
+
 export const metadata = {
   title: "Naranbaatar | Personal Portfolio",
   description: "Full Stack Developer - 6 months experience",
 };
 
-export default function RootLayout() {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body id="Home" className="relative scroll-smooth">
-        <div
-          style={{
-            backgroundImage: "url('/bgdaad.webp')",
-          }}
-          className="absolute w-full h-[120vh] object-contain bg-center"
-        ></div>
-        <div className="min-h-screen min-w-screen bg-black p-[3%]">
-          <Navbar />
-          <Main />
-          <About />
-          <Resume />
-          <Tools />
-          <Contact />
-        </div>
+      <body >
+        {children}
+        
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import RootLayout from "./layout";
+import Navbar from "../Components/header";
+import Main from "../Components/main";
+import About from "@/Components/about";
+import Resume from "../Components/resume";
+import Tools from "@/Components/tools";
+import Contact from "@/Components/contact";
 export default function Home() {
-  
   return (
-    <>
-      <RootLayout />
-    </>
+    <div id="Home" className="relative scroll-smooth">
+      <div
+        style={{
+          backgroundImage: "url('/bgdaad.webp')",
+        }}
+        className="absolute w-full h-[120vh] object-contain bg-center"
+      ></div>
+      <div className="min-h-screen min-w-screen bg-black p-[3%]">
+        <Navbar />
+        <Main />
+        <About />
+        <Resume />
+        <Tools />
+        <Contact />
+      </div>
+    </div>
   );
 }
